41422abadff427db4097df7c14be0e246a0ed05a
othonbreener <othonbrener123@gmail.com>
2024-09-19 10:16:49 -0300

    tests(tests/test_app.py): adiciona teste para rota que busca por id

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,7 +7,6 @@ def test_read_root_deve_retornar_ok(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Hello World'}
 
 
 def test_created_user(client):
@@ -28,6 +27,34 @@ def test_created_user(client):
     }
 
 
+def test_created_user_with_user_name_exists(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'user',
+            'email': 'user@hotmail.com.br',
+            'password': 'password',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Username already exists'}
+
+
+def test_created_user_with_email_exists(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'user2',
+            'email': 'user@hotmail.com.br',
+            'password': 'password',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Email already exists'}
+
+
 def test_read_users(client, user):
     response = client.get(
         '/users/',
@@ -38,6 +65,29 @@ def test_read_users(client, user):
     assert response.json() == [
         {'username': 'user', 'email': 'user@hotmail.com.br', 'id': 1}
     ]
+
+
+def test_read_user_by_id(client, user):
+    response = client.get(
+        '/users/1',
+    )
+
+    assert response.status_code == HTTPStatus.OK
+
+    assert response.json() == {
+        'username': 'user',
+        'email': 'user@hotmail.com.br',
+        'id': 1,
+    }
+
+
+def test_read_user_by_id_not_found(client, user):
+    response = client.get(
+        '/users/2',
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
 
 
 def test_update_user(client, user):
